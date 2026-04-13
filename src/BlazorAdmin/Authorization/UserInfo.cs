3f0574f263d403a0344d50049637a5e7b086f730
DevloperSP <teamrvabalaji@gmail.com>
2026-04-13 15:16:25 +0530

    Refactor UserInfo class for immutability
    Refactor UserInfo properties to use init-only setters for immutability and update Anonymous instantiation.

--- src/BlazorAdmin/Authorization/UserInfo.cs
+++ src/BlazorAdmin/Authorization/UserInfo.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace BlazorAdmin.Authorization;
 
 public class UserInfo
 {
-    public static readonly UserInfo Anonymous = new UserInfo();
-    public bool IsAuthenticated { get; set; }
-    public string NameClaimType { get; set; }
-    public string RoleClaimType { get; set; }
-    public string Token { get; set; }
-    public IEnumerable<ClaimValue> Claims { get; set; }
+    public static readonly UserInfo Anonymous = new();
+
+    public bool IsAuthenticated { get; init; }
+    public string? NameClaimType { get; init; }
+    public string? RoleClaimType { get; init; }
+    public string? Token { get; init; }
+    public IEnumerable<ClaimValue> Claims { get; init; } = new List<ClaimValue>();
 }
